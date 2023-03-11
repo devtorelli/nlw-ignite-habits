a0c5b51f9e1ba2d9b9d4edf8ee9ad234fcfeaf2b
Nicolas Torelli <nicolas.torelli@devtorelli.com.br>
2023-03-11 19:20:07 -0300

    Intergrando o frontend com a API

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -3,14 +3,20 @@ import * as Checkbox from '@radix-ui/react-checkbox';
 import clsx from 'clsx';
 import { ProgressBar } from './ProgressBar';
 import { Check } from 'phosphor-react';
+import dayjs from 'dayjs';
 
 interface HabitDayProps {
-    completed: number
-    amount: number
+    date: Date
+    completed?: number
+    amount?: number
 }
 
-export function HabitDay({ completed, amount}: HabitDayProps) {
-    const completedPercentage = Math.round((completed / amount) * 100)
+export function HabitDay({ completed = 0, amount = 0, date}: HabitDayProps) {
+
+    const completedPercentage = amount > 0 ? Math.round((completed / amount) * 100) : 0
+
+    const dayAndMonth = dayjs(date).format('DD/MM')
+    const dayOfWeek = dayjs(date).format('dddd')
 
     return (
         <Popover.Root>
@@ -22,14 +28,13 @@ export function HabitDay({ completed, amount}: HabitDayProps) {
                     'bg-blue-700 border-blue-500': completedPercentage >= 40 && completedPercentage < 60,
                     'bg-blue-600 border-blue-500': completedPercentage >= 60 && completedPercentage < 80,
                     'bg-blue-500 border-blue-400': completedPercentage >= 80,
-
                 })} 
             />
 
             <Popover.Portal>
                 <Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col">
-                    <span className="font-semibold text-zinc-400">terça-feira</span>
-                    <span className="mt-1 font-extrabold leading-tight text-3xl">17/01</span>
+                    <span className="font-semibold text-zinc-400">{dayOfWeek}</span>
+                    <span className="mt-1 font-extrabold leading-tight text-3xl">{dayAndMonth}</span>
 
                    <ProgressBar progress={completedPercentage}/>
 
